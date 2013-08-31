fc77a0b510e01cb8fbc3e560bd49b3c3f219c250
Igor Entaltsev <igor.yentaltsev@gmail.com>
2013-08-31 13:02:01 -0400

    130831sat 13:02

---
+++ js/tagsmanager.js
@@ -0,0 +1,9 @@
+     //jQuery(".tagManager").tagsManager();
+         jQuery(".tm-input").tagsManager();
+
+         /*jQuery(".tm-input").tagsManager({
+         	typeahead: true,
+         	typeaheadSource: your_function_here(),
+         	blinkBGColor_1: '#FFFF9C',
+         	blinkBGColor_2: '#CDE69C',
+         });*/
