265f8944ee614ee51590b6961b8b9f4c9bb37620
Igor Entaltsev <igor.yentaltsev@gmail.com>
2013-11-08 13:51:44 -0500

    Added Grunt to project 013

--- projects/013/comments.md
+++ projects/013/comments.md
@@ -1,28 +1,28 @@
 comments.md
 ===
-calculator.js
- 
-To use this calculator:
+Gruntfile.js
 
-1) Point your browser to `projects/011/index.html`
+1) the wrapper function: 1/4 components of the Gruntfile  
+4) Project configuration. 2/4 components of the Gruntfile  
+6) grunt.initConfig() method takes in a configuration object in which you define project and task configuration  
+10) the parameter passed to the `initConfig()` is a configuration object  
+12) Import my project’s metadata into the `Grunt config` from my project's `package.json` file  
+20) the object's property is `min` (i.e. the built-in *minification* task). Its value is also an object.  
+22) When you run Grunt from the command line it runs the `grunt-contrib-uglify` plugin's `uglify` task by default.   
+    The `uglify` task is configured to do 2 things:   
+23)  a single `uglify` target: minify a single source file to a single destination file.  
+    can be called `build` or `dist`  
+26) to generate a banner comment dynamically using that metadata.  
+28) `<%== %>` This is how you specify configuration data like filepaths and file lists  
+30) Load the plugin that provides the "uglify" task. Enable the plugin with the commonly used task `grunt-contrib-uglify`  
+40) the source file, the file I want to minify  
+42) specify the default tasks in an array  
+50) the name of the ready-made file. it's location is in the root directory  
+60) pkg.name: its value is taken from package.json/name  
+    pkg.template.today: its value is taken from the templating system, not from package.json  
+70) The banner is a comment at the top of the js file that has useful information about the author etc.   
+    Values are taken from package.json  
 
-2) Open the your browser's console (Mac: Option+Shift+J) 
-
-3) For addition, type: `calculator.add(2,5)`
-
-4) Press `Enter`
-
-===
-10) defining the module, taking the file name as the module name.  
-20) RequireJS, use this folder/file reference (i.e. namespace/modulename reference) to load the filename dependency from this array of dependencies  
-30) map the reference locally (with an arbitrary name, it doesn't have to match the dependancy filename) to the function `console.log()`    
-
-!! the dependency loading order doesn't matter as long as the order in [] is the same as in function():   
-~~~  
-require(['B', 'A', 'C'], function(b, a, c));
-~~~  
-
-40) Why can't I write `['multiply']`? Why do I have to write `['calculator/multiply']`? Both `square.js` and `multiply.js` are in the `calculator` folder.  
 
 
 
--- projects/013/gruntfile.js
+++ projects/013/gruntfile.js
@@ -1,59 +1,30 @@
 //grunt.js
-module.exports = function (grunt) {
-  grunt.initConfig({ // 10
-    min: {// 20
-      dist: { // 30
-        src: 'calculator/add.js',// 40
-        dest: 'add.min.js' // 50
-      }
-    }
-  });
-};
+module.exports = function (grunt) { // 1
+  grunt.initConfig({ // 4, 6, 10
+    
+      pkg: grunt.file.readJSON('package.json'), // 12
+    //=== task 1: jshint ============
+    jshint: { 
+      all: ['calculator/*.js'] 
+      }, // jshint 
+    //=== task 2: uglify =============
+    uglify: { // 22
+      options: { // 70
+        banner: '/*\n<%= grunt.template.today("yyyy-mm-dd")%> <%= pkg.name %> - ' + // 60
+        '<%= pkg.author%> \n*/\n\n'
+      }, // options
+      dist: { // 23
+        src: 'calculator/<%= pkg.name %>.js', // the file to be uglified
+        dest: '<%= pkg.name %>_<%= pkg.version %>.min.js' // the resultant file
+      } // dist
+    }// uglify
+    //=========================
+  }); // grunt.initConfig
 
-/*
+  grunt.loadNpmTasks('grunt-contrib-uglify'); // 30  
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-10) the parameter of the `initConfig()` is a configuration object (A)
-20) the object's property is `min` (i.e. the built-in *minification* task). Its value is also an object (AA).
-30) 
-40) the source file, the file I want to minify
-50) the name of the ready-made file. it's location is in the root directory
+  grunt.registerTask('default', ['uglify', 'jshint']); // 42
 
-*/
+}; // module.exports
 
-module.exports = function(grunt) { // 1
-
-  // Project configuration. // 4
-    grunt.initConfig({ // 6 
-      pkg: grunt.file.readJSON('package.json'), // 10
-        uglify: { // 20
-          options: { // 26
-            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' //28
-          }, // options
-          build: { // 23
-            src: 'src/<%= pkg.name %>.js',
-            dest: 'build/<%= pkg.name %>.min.js'
-          } //build
-        } // uglify
-     });
-
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify'); // 30
-
-    // Default task(s).
-    grunt.registerTask('default', ['uglify']); // 40
-};
-
-/*
-1) the wrapper function: 1/4 components of the Gruntfile
-4) 2/4 components of the Gruntfile
-6) grunt.initConfig() method takes in a configuration object in which you define project and task configuration
-10) Your project’s metadata is imported into the `Grunt config` from your project's `package.json` file
-20) When you run Grunt from the command line it runs the `grunt-contrib-uglify` plugin's `uglify` task by default. 
-    The `uglify` task is configured to do 2 things: 
-23)  a single `uglify` target: minify a single source file to a single destination file.
-26) to generate a banner comment dynamically using that metadata.
-28) <%== %> This is how you specify configuration data like filepaths and file lists
-30) Enable the plugin with the commonly used task `grunt-contrib-uglify`
-40) specify the default tasks in an array
-
-*/
